6d912c0a7c3d1625b57d4878b71af60cc9b83069
Raoul <raouldegodewarsvelde.thetrueone@gmail.com>
2023-01-09 16:32:29 +0100

    31'19: python manage.py runserver 8000

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,8 +1,9 @@
 import pip._vendor.requests 
 
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://127.0.0.1:8000/"
 
 get_response = pip._vendor.requests.get(endpoint, json={"query": "Hello World!"})
 print(get_response.text)
-print(get_response.json())
+# print(get_response.json())
 print(get_response.status_code)
